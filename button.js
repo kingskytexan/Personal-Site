c0cd789e1ca9e89acf9907c398e15238f845de22
Thomas B Klapheke <tommyklapheke@yahoo.com>
2017-07-20 13:30:15 -0400

    Made slight changes to comments while giving it one more look over. See some things I would like to change down the road whne I have the knowledge

--- button.js
+++ button.js
@@ -1,6 +1,8 @@
 //This JavaScript script runs when you click the submit 
 //	button will display to alert boxes in a row 
-//	if you click on OK
+//	if you click on OK. If you don't click on OK
+//	it will close the first box and not continue onto the 
+//	second one.
 
 document
     .getElementById("confirmClickActionElementId")
